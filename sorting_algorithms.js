60555508494f6f5c0a57e1d88bbfd08c0e40ee15
Joey Doyle <j.doyle345@gmail.com>
2018-03-20 15:47:02 -0700

    Add quickSort to the sorting algorithms file

--- sorting_algorithms.js
+++ sorting_algorithms.js
@@ -1,5 +1,5 @@
 // Binary Search
-// --------------
+// ----------------
 // Given a sorted array and a target value, find out whether or not the
 // target value is in the array
 
@@ -27,29 +27,59 @@ var binarySearch = (array, target) => {
 };
 
 
-// var mergeSort = function(array) {
-//   if (array.length < 2) {
-//     return array;
-//   } else {
-//     const middle = Math.floor(array.length / 2);
-//     const left = mergeSort(array.slice(0, middle));
-//     const right = mergeSort(array.slice(middle));
-//
-//     merge(left, right);
-//   }
-// };
-//
-// function merge(left, right) {
-//   const merged = Array.new;
-//
-//   while (left.length > 0 && right.length > 0){
-//     const mergeVal = (left[0] < right[0]) ? left.shift() : right.shift();
-//     merged.push(mergeVal);
-//   }
-//
-//   return merged.concat(left, right);
-// }
+// Merge Sort
+// ----------------
+var mergeSort = function(array) {
+  if (array.length < 2) {
+    return array;
+  } else {
+    const middle = Math.floor(array.length / 2);
+    const left = mergeSort(array.slice(0, middle));
+    const right = mergeSort(array.slice(middle));
 
-var mergeSort = (array) => {
+    merge(left, right);
+  }
+};
 
+function merge(left, right) {
+  const merged = Array.new;
+
+  while (left.length > 0 && right.length > 0){
+    const mergeVal = (left[0] < right[0]) ? left.shift() : right.shift();
+    merged.push(mergeVal);
+  }
+
+  return merged.concat(left, right);
+}
+
+
+// Quick Sort
+// ----------------
+var quickSort = (array) => {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const pivot = array[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (comparator(array[i], pivot) === -1) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+
+  return quickSort(left).concat(pivot).concat(quickSort(right));
+
+};
+
+var comparator = (num, pivot) => {
+  if (num < pivot) {
+    return -1;
+  } else {
+    return 1;
+  }
 };
