b7038804bfe2b724952108ddd2fbe05ee5d088d1
Joey Doyle <j.doyle345@gmail.com>
2018-03-20 12:03:02 -0700

    Add merge sort to sorting algorithms

--- sorting_algorithms.js
+++ sorting_algorithms.js
@@ -3,46 +3,53 @@
 // Given a sorted array and a target value, find out whether or not the
 // target value is in the array
 
-// var binarySearch = (array, target) => {
-//   if (array.length === 0) {
-//     return -1;
-//   }
-//
-//   var pivotIdx = Math.floor(array.length / 2);
-//   var pivotVal = array[pivotIdx];
-//
-//   if (target === pivotVal) {
-//     return pivotIdx;
-//   } else if (target < pivotVal) {
-//     var left = array.slice(0, pivotIdx);
-//     return binarySearch(left, target);
-//   } else {
-//     var right = array.slice(pivotIdx + 1);
-//     const rightSide = binarySearch(right, target);
-//     return rightSide === -1 ? -1 : rightSide + pivotIdx + 1;
-//   }
-//
-// };
-
-//
 // [1,2,3,4,5,6,7]
 
-var bSearch = (array, target) => {
-  if (array.length < 1) {
+var binarySearch = (array, target) => {
+  if (array.length === 0) {
     return -1;
   }
 
-  const pivotIdx = Math.floor(array.length / 2);
-  const pivotVal = array[pivotIdx];
+  var pivotIdx = Math.floor(array.length / 2);
+  var pivotVal = array[pivotIdx];
 
-  if (pivotVal === target) {
+  if (target === pivotVal) {
     return pivotIdx;
-  } else if (pivotVal < target) {
-    const left = array.slice(0, pivotIdx);
-    return bSearch(left, target);
-  } else if (pivotVal > target) {
-    const right = array.slice(pivotIdx + 1);
-    const rightSide = bSearch(right, target);
+  } else if (target < pivotVal) {
+    var left = array.slice(0, pivotIdx);
+    return binarySearch(left, target);
+  } else {
+    var right = array.slice(pivotIdx + 1);
+    const rightSide = binarySearch(right, target);
     return rightSide === -1 ? -1 : rightSide + pivotIdx + 1;
   }
+
+};
+
+
+// var mergeSort = function(array) {
+//   if (array.length < 2) {
+//     return array;
+//   } else {
+//     const middle = Math.floor(array.length / 2);
+//     const left = mergeSort(array.slice(0, middle));
+//     const right = mergeSort(array.slice(middle));
+//
+//     merge(left, right);
+//   }
+// };
+//
+// function merge(left, right) {
+//   const merged = Array.new;
+//
+//   while (left.length > 0 && right.length > 0){
+//     const mergeVal = (left[0] < right[0]) ? left.shift() : right.shift();
+//     merged.push(mergeVal);
+//   }
+//
+//   return merged.concat(left, right);
+// }
+
+var mergeSort = (array) => {
+
 };
